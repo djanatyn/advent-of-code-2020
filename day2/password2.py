8f57c13f5260e352f82b993a01b81a84802c7fd7
Jonathan Strickland <djanatyn@gmail.com>
2020-12-03 18:02:07 -0500

    day2: second problem

---
+++ day2/password2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import re
+
+class Record(object):
+    def __init__(self, index1, index2, matchChar, matchText):
+        self.indexes = [index1, index2]
+        self.matchChar = matchChar
+        self.matchText = matchText
+
+
+def parse(line):
+    regex = r'(\d+)\-(\d+) (\w)\: (\w+)$'
+    match = re.match(regex, line)
+
+    return Record(
+        int(match.groups()[0]),
+        int(match.groups()[1]),
+        match.groups()[2],
+        match.groups()[3],
+    )
+
+def validate(record):
+    matches = [record.matchText[index - 1] for index in record.indexes]
+    return matches.count(record.matchChar) == 1
+
+
+def load(filename):
+    return [parse(line) for line in open(filename, 'r')]
+
+
+def main():
+    validation = [validate(record) for record in load("input")]
+
+    print(f"valid records: {validation.count(True)}")
+    print(f"invalid records: {validation.count(False)}")
+
+
+if __name__ == '__main__':
+    main()
+
