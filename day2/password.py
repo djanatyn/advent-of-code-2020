15f24dc4d7dcd35bd762238f24f5986b8a63f33c
Jonathan Strickland <djanatyn@gmail.com>
2020-12-03 17:54:56 -0500

    initial commit: day 2???
    i did day 1 interactively, so i still need to write a file!

---
+++ day2/password.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import re
+
+class Record(object):
+    def __init__(self, lowerBound, upperBound, matchChar, matchText):
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+        self.matchChar = matchChar
+        self.matchText = matchText
+
+
+def parse(line):
+    regex = r'(\d+)\-(\d+) (\w)\: (\w+)$'
+    match = re.match(regex, line)
+
+    return Record(
+        int(match.groups()[0]),
+        int(match.groups()[1]),
+        match.groups()[2],
+        match.groups()[3],
+    )
+
+def validate(record):
+    occurrences = record.matchText.count(record.matchChar)
+    return record.lowerBound <= occurrences <= record.upperBound
+
+
+def load(filename):
+    return [parse(line) for line in open(filename, 'r')]
+
+
+def main():
+    validation = [validate(record) for record in load("input")]
+
+    print(f"valid records: {validation.count(True)}")
+    print(f"invalid records: {validation.count(False)}")
+
+
+if __name__ == '__main__':
+    main()
